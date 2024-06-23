8042a9c83756c96f25264cb50b08624b5da5ee8f
Elcho911 <boss.armsport@gmail.com>
2024-06-23 06:10:57 +0600

    fix bug with preloader

--- src/ui/preLoader/Preloader.tsx
+++ src/ui/preLoader/Preloader.tsx
@@ -1,27 +1,9 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import scss from './PreLoader.module.scss';
-// import { Dna } from "react-loader-spinner";
 
-interface PreloaderType {
-	className?: any;
-}
-
-const Preloader: FC<PreloaderType> = ({ className }) => {
+const Preloader: FC = () => {
 	return (
 		<div className={scss.loader}>
-			{/* V1 */}
-			{/* <div className={scss.pre_loader}>
-				<Dna
-					visible={true}
-					height="225"
-					width="225"
-					ariaLabel="dna-loading"
-					wrapperStyle={{}}
-					wrapperClass="dna-wrapper"
-				/>
-			</div> */}
-
-			{/* V2 */}
 			<div className={scss.infinityChrome}>
 				<div></div>
 				<div></div>
