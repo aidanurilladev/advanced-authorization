4842168128efde3f3c64b4ff58d21b988937d456
Elcho911 <boss.armsport@gmail.com>
2024-06-23 06:08:57 +0600

    added preloadr

--- src/pagesSide/components/layout/LayoutSide.tsx
+++ src/pagesSide/components/layout/LayoutSide.tsx
@@ -5,10 +5,11 @@ import Header from './header/Header.tsx';
 import Footer from './footer/Footer.tsx';
 import SideBar from '@/src/pagesSide/components/layout/sideBar/SideBar.tsx';
 import { useGetMeQuery } from '@/src/redux/api/auth';
+import Preloader from '@/src/ui/preLoader/Preloader.tsx';
 
 const LayoutSide = () => {
-	const { data } = useGetMeQuery();
-	console.log(data);
+	const { data, status } = useGetMeQuery();
+	const [isPreLoader, setIsPreloader] = useState(true);
 	const [isOpen, setIsOpen] = useState(false);
 	const [isMobile, setIsMobile] = useState(false);
 
@@ -21,31 +22,47 @@ const LayoutSide = () => {
 		};
 	}, []);
 
+	useEffect(() => {
+		if (status === 'fulfilled' || status === 'rejected') {
+			setIsPreloader(false);
+		}
+	}, [status]);
+
 	return (
 		<>
-			<div className={scss.layout} onClick={() => setIsOpen(false)}>
-				<Header
-					isOpen={isOpen}
-					setIsOpen={setIsOpen}
-					isMobile={isMobile}
-					user={data!}
-				/>
-				<main>
-					<div className="container">
-						<div className={scss.content}>
-							{!isMobile && (
-								<div className={scss.side_bar}>
-									<SideBar />
+			{isPreLoader ? (
+				<>
+					<Preloader />
+				</>
+			) : (
+				<>
+					<div className={scss.layout} onClick={() => setIsOpen(false)}>
+						{data && (
+							<Header
+								isOpen={isOpen}
+								setIsOpen={setIsOpen}
+								isMobile={isMobile}
+								user={data.profile!}
+							/>
+						)}
+						<main>
+							<div className="container">
+								<div className={scss.content}>
+									{!isMobile && (
+										<div className={scss.side_bar}>
+											<SideBar />
+										</div>
+									)}
+									<div className={scss.page_body}>
+										<Outlet />
+									</div>
 								</div>
-							)}
-							<div className={scss.page_body}>
-								<Outlet />
 							</div>
-						</div>
+						</main>
+						<Footer />
 					</div>
-				</main>
-				<Footer />
-			</div>
+				</>
+			)}
 		</>
 	);
 };
